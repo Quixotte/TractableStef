c7468914be287251ab900ce7ecaca594d060e0c3
Stef Janssen <steffos.janssen@gmail.com>
2015-07-15 16:52:53 +0200

    attempt at reading in hdf5 data

---
+++ learn_and_test.py
@@ -0,0 +1,30 @@
+
+from sklearn import cross_validation
+from numpy import genfromtxt, savetxt
+from sklearn.cross_validation import train_test_split
+
+import caffe
+
+import os
+import h5py
+import shutil
+import csv
+
+import numpy
+
+def learn_and_test(solver_file):
+    caffe.set_mode_cpu()
+    solver = caffe.get_solver(solver_file)
+    solver.solve()
+
+    accuracy = 0
+    test_iters = int(len(Xt) / solver.test_nets[0].blobs['data'].num)
+    for i in range(test_iters):
+        solver.test_nets[0].forward()
+        accuracy += solver.test_nets[0].blobs['accuracy'].data
+    accuracy /= test_iters
+    return accuracy
+
+if __name__ == "__main__":
+    learn_and_test('stef_solver')
+    
