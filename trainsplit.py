b278b02c8a4ce76bf34e536ff729713d9d14dee0
Stef Janssen <steffos.janssen@gmail.com>
2015-07-16 22:20:49 +0200

    reading img now prints

--- trainsplit.py
+++ trainsplit.py
@@ -128,8 +128,10 @@ def main():
     image_dir = os.path.join(base, "res_imgs/")
 
     #load images
-    train_images = [load_one_image(os.path.join(image_dir, d)) for d in data_train if os.path.exists(image_dir + d)]
-    print train_images.shape
+    train_images = [load_one_image(os.path.join(image_dir, d), do_print=True) for d in data_train if os.path.exists(image_dir + d)]
+
+
+
     #test_images = [load_one_image(image_dir + d) for d in data_test]
 
     hd5_train_images_filename = os.path.join(base, "hd5_images_train")
