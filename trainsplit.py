22bc98f2a6c4eb774cceb12d14aedd76e01d328d
Stef Janssen <steffos.janssen@gmail.com>
2015-07-17 12:15:14 +0200

    fixed indent mistake

--- trainsplit.py
+++ trainsplit.py
@@ -156,12 +156,12 @@ def main():
     print 'Shape of labels:'
     print train_labels.shape
 
-     h5py.File(hd5_train_images_filename, 'w') as f:
+    h5py.File(hd5_train_images_filename, 'w') as f:
         #f['data'] = train_images.astype(numpy.float32)
         #f['label'] = labels_train.astype(numpy.float32)
         f.create_dataset("data", train_images.shape , compression='gzip', compression_opts=1, dtype=numpy.float32, data=train_images)
         f.create_dataset("label", train_labels.shape , compression='gzip', compression_opts=1, dtype=numpy.float32, data=train_labels)
-    
+
     #with open(hd5_meta_train, 'w') as f:
      #   f.write(hd5_train_images_filename + '\n')
       #  f.write(hd5_train_images_filename + '\n')
