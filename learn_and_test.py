eed3ca1181ff488d0d631dfc190331fd16116638
Stef Janssen <steffos.janssen@gmail.com>
2015-07-17 21:07:50 +0200

    trying to just debug, not wanting to wait a long time till finished

--- learn_and_test.py
+++ learn_and_test.py
@@ -15,15 +15,15 @@ import numpy
 def learn_and_test(solver_file):
     caffe.set_mode_cpu()
     solver = caffe.get_solver(solver_file)
-    solver.solve()
+    solver.step(1)
 
-    accuracy = 0
-    test_iters = int(len(Xt) / solver.test_nets[0].blobs['data'].num)
-    for i in range(test_iters):
-        solver.test_nets[0].forward()
-        accuracy += solver.test_nets[0].blobs['accuracy'].data
-    accuracy /= test_iters
-    return accuracy
+    #accuracy = 0
+    #test_iters = int(len(Xt) / solver.test_nets[0].blobs['data'].num)
+    #for i in range(test_iters):
+    #    solver.test_nets[0].forward()
+    #    accuracy += solver.test_nets[0].blobs['accuracy'].data
+    #accuracy /= test_iters
+    #return accuracy
 
 if __name__ == "__main__":
     learn_and_test('stef_solver.prototxt')
