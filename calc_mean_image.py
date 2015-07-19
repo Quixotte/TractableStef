fe12c816d97fdcbfca0316577d4fad0ee06e42b2
Stef Janssen <steffos.janssen@gmail.com>
2015-07-19 14:00:00 +0200

    removed unnecessary label loading

--- calc_mean_image.py
+++ calc_mean_image.py
@@ -25,30 +25,12 @@ def load_one_image(filename, do_print = False):
 def main():
     base = '/data/ad6813/Nus-wide/TractableStef'
 
-    file_labels = os.path.join(base, 'labels.txt')
-
-    label2num = {'lake':1, 'plants':2, 'window':3, 'buildings':4, 'grass':5, 'animal':6, 'water':7, 'person':8, 'clouds':9, 'sky':10, 'NA':0}
-
     dataset = trainsplit.importCSV(file_labels, delimiterChar=' ')
 
     data_all   = [x[0] for x in dataset]
-    labels_all = [x[1:] for x in dataset]
-    labels_all = [map(lambda label: label2num[label], x) for x in labels_all]
 
-    labels_binary = numpy.zeros((len(labels_all), len(label2num)), dtype=numpy.float32)
-    for (i, v_label) in enumerate(labels_all):
-        # label_coded = [0] * 11
-        for num in v_label:
-            labels_binary[i,num] = 1.0
-
-        # labels_binary[i] = label_coded
-        # labels_binary[i] = numpy.array(label_coded, dtype=numpy.uint8)
-
-    print 'First few labels:', labels_binary[0:10]
-    print '==='
 
     data_train = data_all
-    labels_train = labels_binary
 
     # TODO: doesn't work with vector labels
     # data_train, data_test, labels_train, labels_test = train_test_split(data_all, labels_all, test_size=0.20, random_state=42)
@@ -79,7 +61,7 @@ def main():
             print "Couldn't find file", d
 
     mean_image = numpy.mean(images)
-    numpy_file_name = os.path.join(base, "mean_image")
+    numpy_file_name = os.path.join(base, "mean_image.npy")
     numpy.save(numpy_file_name, mean_image)
 
     loaded_img = numpy.load(numpy_file_name)
