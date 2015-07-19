d554dc60a3128a92d8a9b3ab52424c30beface72
Stef Janssen <steffos.janssen@gmail.com>
2015-07-19 12:01:00 +0200

    trying to access blobs from python

--- learn_and_test.py
+++ learn_and_test.py
@@ -13,9 +13,16 @@ import csv
 import numpy
 
 def learn_and_test(solver_file):
-    caffe.set_mode_gpu()
-    solver = caffe.get_solver(solver_file)
-    solver.step(100)
+
+    net = caffe.Net(solver_file, 'weights.caffemodel')
+    net.forward() # this will load the next mini-batch as defined in the net
+    label1 = net.blobs['label1'].data # or whatever you want
+
+    print label1
+
+    #caffe.set_mode_gpu()
+    #solver = caffe.get_solver(solver_file)
+    #solver.step(100)
 
     #accuracy = 0
     #test_iters = int(len(Xt) / solver.test_nets[0].blobs['data'].num)
