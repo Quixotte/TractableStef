7b80d2f14239333b8ae44ec13cd7c5a47dd9f90a
Stef Janssen <steffos.janssen@gmail.com>
2015-07-22 22:57:19 +0200

    passing net file instead of solver file

--- learn_and_test.py
+++ learn_and_test.py
@@ -32,11 +32,11 @@ def learn_and_test(solver_file, label_num):
     #accuracy /= test_iters
     #return accuracy
 
-def get_advanced_accuracy(solver_file, caffe_model, label_num):
+def get_advanced_accuracy(net_file, caffe_model, label_num):
 
-    print solver_file
+    print net_file
     print caffe_model
-    net = caffe.Net(solver_file, caffe_model)
+    net = caffe.Net(net_file, caffe_model)
 
     caffe.set_mode_gpu()
     caffe.set_phase_test()
@@ -77,12 +77,12 @@ if __name__ == "__main__":
 
     ##In order for this to work the batch size of the test has to be 1.
 
-    solver_base = 'binary_solvers/binary_stef_solver_'
+    net_base = 'binary_solvers/binary_stef_net_'
     model_base = '../../../../tmp/stef_net/snapshot_file_binary_'
     for i in numpy.arange(1,2):
-        solver_file = solver_base + str(i) + '.prototxt'
+        net_file = net_base + str(i) + '.prototxt'
         snapshot_file = model_base + str(i) + '.caffemodel'
-        get_advanced_accuracy(solver_file, snapshot_file, i)
+        get_advanced_accuracy(net_file, snapshot_file, i)
 
 
 
