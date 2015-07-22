21fbde288e3062f6c2e5127582fd86610ba3884b
Stef Janssen <steffos.janssen@gmail.com>
2015-07-22 23:46:31 +0200

    gonna train a whole lotta networks

--- learn_and_test.py
+++ learn_and_test.py
@@ -22,7 +22,7 @@ def learn(solver_file, label_num):
     print 'currently solving for label number: ' + str(label_num)
     caffe.set_mode_gpu()
     solver = caffe.get_solver(solver_file)
-    solver.step(101)
+    solver.step(10001)
 
 def get_advanced_accuracy(net_file, caffe_model, label_num):
 
@@ -62,13 +62,13 @@ def get_advanced_accuracy(net_file, caffe_model, label_num):
         f.write("Negative accuracy rate: " + str(neg_acc))
 
 
-if __name__ == "__main__2":
+if __name__ == "__main__":
     base = 'binary_solvers/binary_stef_solver_'
     for i in numpy.arange(1,2):
         file_name = base + str(i) + '.prototxt'
         learn(file_name, i)
 
-if __name__ == "__main__":
+if __name__ == "__main__1":
 
     ##In order for this to work the batch size of the test has to be 1.
 
